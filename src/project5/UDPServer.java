42ffc46d09dd23bebd8027bee6c5e7a9875b5917
BoWen <WenTesla@163.com>
2022-10-31 14:29:44 +0800

    实验五（基本框架）

---
+++ src/project5/UDPServer.java
@@ -0,0 +1,64 @@
+package project5;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author BoWen
+ * @version 1.0
+ * @date 2022/10/31
+ */
+
+public abstract class UDPServer implements Runnable {
+    //字节
+    private int bufferSize;
+    //端口
+    private final int port;
+    //日志
+    private final Logger logger=Logger.getLogger(UDPServer.class.getCanonicalName());
+    //是否关闭
+    private volatile boolean isShutDown=false;
+
+    public UDPServer(int bufferSize,int port) {
+        this.bufferSize=bufferSize;
+        this.port=port;
+
+    }
+
+    public UDPServer(int port){
+        this(8192,port);
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[bufferSize];
+        try (DatagramSocket socket = new DatagramSocket(port)){
+            logger.log(Level.INFO,"服务器启动成功！");
+            while (true) {
+                //先检查变量
+                if (isShutDown) return;
+                //
+                DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+                socket.receive(incoming);
+                this.respond(socket,incoming);
+            }
+        } catch (SocketException e) {
+            logger.log(Level.SEVERE,"服务器启动端口:"+port+"失败");
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.log(Level.WARNING,e.getMessage(),e);
+            e.printStackTrace();
+        }
+    }
+    //抽象类一定定义抽象方法
+    protected abstract void respond(DatagramSocket socket, DatagramPacket incoming);
+
+    protected void shuDown(){
+        isShutDown=true;
+    }
+
+}
